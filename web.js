9b0420d9768f1ace388adda5d91b686b9a6226d8
Adarsh Hasija <ubuntu@ip-10-166-219-4.us-west-1.compute.internal>
2013-07-11 02:24:27 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var content = fs.readFileSync('index.html');
+  content.toString();
+  var buffer = new Buffer();
+  buffer.write(content);
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
